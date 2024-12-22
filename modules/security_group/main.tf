4ac4682c3c783be63c4fd4b6eff5785c7a3346ac
SoYeon Kim <61140818+thdusqpf@users.noreply.github.com>
2024-12-22 21:07:37 +0900

    security group 모듈 생성

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -14,7 +14,7 @@ resource "aws_vpc_security_group_ingress_rule" "ingress" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "egress" {
-  security_group_id            = aws_security_group.this.id
+  security_group_id            = aws_security_group.song-sg-an2.id
   for_each                     = { for idx, rule in var.egress_rules : "${idx}_${rule.from_port}_${rule.to_port}" => rule }
   from_port                    = each.value.protocol == "all" ? null : each.value.from_port
   to_port                      = each.value.protocol == "all" ? null : each.value.to_port
